f43cb0a9675c428e075a06499cb795d0880cfa0b
amirreza vafaei <110279643+Amirreza74Vafaei@users.noreply.github.com>
2025-05-29 01:31:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -152,6 +152,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     data = query.data
+
     if data == 'full_report':
         report = []
         for symbol in SYMBOLS:
